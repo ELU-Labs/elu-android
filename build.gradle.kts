ebefa47e69a53bcc86ad3630df056ba4f1381047
aliajam <ali.ajam90@yahoo.com>
2026-07-31 22:48:44 -0700

    Bump AGP 8.9.2 -> 8.13.2 (required by maven-publish plugin 0.37.0)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
-    id("com.android.library") version "8.9.2" apply false
+    // vanniktech maven-publish 0.37.0 requires AGP >= 8.13.0 (checked at apply).
+    id("com.android.library") version "8.13.2" apply false
     id("org.jetbrains.kotlin.android") version "2.1.20" apply false
     id("com.vanniktech.maven.publish") version "0.37.0" apply false
 }
